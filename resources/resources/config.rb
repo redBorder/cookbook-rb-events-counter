5bc02c0d191ff6efa51976271c8e453b91d1e818
Miguel Negron <manegron@redborder.com>
2024-06-01 02:44:45 +0100

    lint resources

--- resources/resources/config.rb
+++ resources/resources/config.rb
@@ -1,17 +1,8 @@
-# Cookbook Name:: rbevents-counter
-#
+# Cookbook:: rbevents-counter
 # Resource:: config
-#
 
 actions :add, :remove, :register, :deregister
 default_action :add
 
-attribute :user, :kind_of => String, :default => "redborder-events-counter"
-attribute :cdomain, :kind_of => String, :default => "redborder.cluster"
-
-# EXAMPLES
-
-#attribute :myinteger, :kind_of => Fixnum, :default => 1
-#attribute :myarray, :kind_of => Array, :default => ["val1"]
-#attribute :myhash, :kind_of => Object, :default => {"val1" => "1"}
-#attribute :myboolean, :kind_of => [TrueClass, FalseClass], :default => true
+attribute :user, kind_of: String, default: 'redborder-events-counter'
+attribute :cdomain, kind_of: String, default: 'redborder.cluster'
