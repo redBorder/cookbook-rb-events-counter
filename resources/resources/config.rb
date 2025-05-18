80c25f44321bab7fe1f62efcda2c00faea75e27a
Rafa Gómez <rgomez@redborder.com>
2025-05-18 10:18:18 +0100

    Feat: Creating helper method that returns current and removed licenses. Fix: Removed licenses are removed from /etc/licenses

--- resources/resources/config.rb
+++ resources/resources/config.rb
@@ -6,3 +6,4 @@ default_action :add
 
 attribute :user, kind_of: String, default: 'redborder-events-counter'
 attribute :cdomain, kind_of: String, default: 'redborder.cluster'
+attribute :licenses_dir, kind_of: String, default: '/etc/licenses'
