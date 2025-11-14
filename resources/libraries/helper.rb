0b97dda1b2c8218b36a65630e620b143fdafe309
manegron <manegron@redborder.com>
2025-11-14 12:51:14 +0000

    Update helper

--- resources/libraries/helper.rb
+++ resources/libraries/helper.rb
@@ -1,10 +1,13 @@
 module RbEventscounter
   module Helper
     def get_license_info(licenses_dir)
+      could_not_get_licenses_databag = false
+
       begin
         licenses_dg = data_bag_item('rBglobal', 'licenses')
       rescue
         licenses_dg = { 'licenses' => {} }
+        could_not_get_licenses_databag = true
       end
 
       current_licenses = licenses_dg['licenses'] || {}
@@ -15,7 +18,8 @@ module RbEventscounter
                             []
                           end
 
-      licenses_to_remove = existing_licenses - current_licenses.keys
+      licenses_to_remove = []
+      licenses_to_remove = existing_licenses - current_licenses.keys unless could_not_get_licenses_databag
 
       {
         current_licenses: current_licenses,
